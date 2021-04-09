beef2720388cba83deaa698841f2b97573514cf5
Leonardo <leoprofissional@live.com>
2021-04-09 07:30:33 -0300

    first commit

---
+++ psr-3/vendor/composer/installed.php
@@ -0,0 +1,49 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'eti/test-monolog',
+  ),
+  'versions' => 
+  array (
+    'eti/test-monolog' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd8c787753b3a2ad11bc60c063cff1358a32a3b4',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4ebe3a8bf773a19edfe0a84b6585ba3d401b724d',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+  ),
+);
